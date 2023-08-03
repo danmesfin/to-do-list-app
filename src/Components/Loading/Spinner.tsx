a7bdea7557d09683fb200ff7a6262866ad88145e
danmesfin <danielmsfn@gmail.com>
2023-08-04 00:57:43 +0300

    added custom loading spinner

---
+++ src/Components/Loading/Spinner.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Loading: React.FC = () => {
+  return (
+    <div className="w-6 h-6 border-t-4 border-blue-500 border-solid rounded-full animate-spin"></div>
+  );
+};
+
+export default Loading;
--- src/Components/signUp/index.tsx
+++ src/Components/signUp/index.tsx
@@ -7,28 +7,59 @@ import {
   UserCredential,
 } from "firebase/auth";
 import { auth } from "../../../firebaseConfig";
-import { UserIcon, LockClosedIcon } from "@heroicons/react/24/solid"; // Import the required HeroIcons
+import { UserIcon, LockClosedIcon } from "@heroicons/react/24/solid";
+import { useRouter } from "next/navigation";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Loading from "../Loading/Spinner"; // Custom loading spinner component
 
 const SignupForm: React.FC = () => {
   const dispatch = useDispatch();
+  const router = useRouter();
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleSignup = async (e: FormEvent) => {
     e.preventDefault();
+    setIsLoading(true);
+
     try {
       const userCredential: UserCredential =
         await createUserWithEmailAndPassword(auth, email, password);
       const user = userCredential.user;
       dispatch(setCurrentUser(user));
+
+      setIsLoading(false);
+      toast.success("Sign up successful!", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+
+      // Redirect to the dashboard after successful sign-up
+      router.push("/dashboard");
     } catch (error: any) {
-      console.error("Error signing up:", error.message);
+      setIsLoading(false);
+      toast.error(error.message, {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
     }
   };
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <h1 className="text-2xl font-bold mb-4 text-center">Sign Up</h1>
+      <h1 className="text-2xl font-bold mb-4">Sign Up</h1>
       <form
         className="flex flex-col w-72 rounded p-4 items-center"
         onSubmit={handleSignup}
@@ -55,11 +86,12 @@ const SignupForm: React.FC = () => {
         </div>
         <button
           type="submit"
-          className="w-full bg-green-500 text-white px-4 py-2 rounded-full hover:bg-green-600"
+          className="w-full bg-green-500 text-white px-4 py-2 rounded-full hover:bg-green-600 item-center"
         >
-          Sign Up
+          {isLoading ? <Loading /> : "Sign Up"}
         </button>
       </form>
+      <ToastContainer />
     </div>
   );
 };
